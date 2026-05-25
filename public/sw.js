21e053551e1a4ea4e75902e32aacce89d524f1d1
Zawadi Sanga <zawadisanga397@gmail.com>
2026-05-26 00:21:56 +0300

    Simplify service worker cache management

--- public/sw.js
+++ public/sw.js
@@ -1,30 +1,10 @@
 const CACHE_NAME = 'cityfind-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/dashboard-admin.html',
-  '/dashboard-company.html',
-  '/dashboard-provider.html',
-  '/dashboard-receiver.html',
-  '/product.html'
-];
+const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
