cb7fabf518a308e3315153bffbb188b9a0b330da
ALS <zawadisanga397@gmail.com>
2026-06-27 00:57:19 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'cityfind-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard-buyer.html',
+  '/dashboard-sellers.html',
+  '/dashboard-admin.html',
+  '/product.html',
+  '/manifest.json',
+  '/icon-72x72.png',
+  '/icon-96x96.png',
+  '/icon-128x128.png',
+  '/icon-144x144.png',
+  '/icon-152x152.png',
+  '/icon-192x192.png',
+  '/icon-384x384.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
